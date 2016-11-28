58c8d80c7b7251d429bd7c767721c48e318078c3
Robert Lukierski <robert@lukierski.eu>
2016-11-28 20:55:01 +0000

    FindV4L too harsh

--- scripts/FindV4L2.cmake
+++ scripts/FindV4L2.cmake
@@ -11,8 +11,12 @@
 #  V4L2_ROOT - Can be set to V4L2 install path or Windows build path
 #
 
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(PC_V4L2 REQUIRED libv4l2)
+find_package(PkgConfig)
+if(${CMAKE_VERSION} VERSION_LESS 2.8.2)
+    pkg_check_modules(PC_V4L2 libv4l2)
+else()
+    pkg_check_modules(PC_V4L2 QUIET libv4l2)
+endif()
 if(PC_V4L2_FOUND)
     set(V4L2_FOUND TRUE)
     set(V4L2_INCLUDE_DIRS ${PC_V4L2_INCLUDE_DIRS})
